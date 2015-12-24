24b775acb9a9d326c1001e181559d2df9b5e7e81
osher <osherztikz@gmail.com>
2015-12-24 22:43:35 +0200

    Added Empty main

---
+++ A16 Ex03/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A16_Ex03
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
