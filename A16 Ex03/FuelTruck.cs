ae991fa6de7be85f60e7d6206d62196c0e95fc60
egabay <edvin4112@gmail.com>
2015-12-24 15:28:58 +0200

    Again

---
+++ A16 Ex03/FuelTruck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A16_Ex03
+{
+    class FuelTruck : FuelVehicle
+    {
+        protected const int k_NumberOfWheels = 12;
+        internal static float s_MaximumAirPressure = 34; 
+        internal static Enums.eFuelType s_FuelType = Enums.eFuelType.Soler;
+        internal static float s_LitersFuelTank = 160;
+        protected float m_MaximumCarryingWeight;
+        protected bool m_IsCarryDangerousMaterials;
+
+
+        public FuelTruck(string i_ModelName, string i_LicenseNumber, float i_RemainingEnergyPercent, 
+            string i_WheelManufacturName, float i_CurrentAirPressure, float i_CurrentFuelAmountByLiters, 
+            float mMaximumCarryingWeight, bool mIsCarryDangerousMaterials) :
+            base(i_ModelName, i_LicenseNumber, i_RemainingEnergyPercent, k_NumberOfWheels, i_WheelManufacturName,
+            i_CurrentAirPressure, s_MaximumAirPressure, s_FuelType, i_CurrentFuelAmountByLiters, s_LitersFuelTank)
+        {
+            m_MaximumCarryingWeight = mMaximumCarryingWeight;
+            m_IsCarryDangerousMaterials = mIsCarryDangerousMaterials;
+        }
+
+        public float TruckMaximumCarryingWeight
+        {
+            get { return m_MaximumCarryingWeight; }
+            set { m_MaximumCarryingWeight = value; }
+        }
+
+        public bool IsCarryDangerousMaterials
+        {
+            get { return m_IsCarryDangerousMaterials; }
+            set { m_IsCarryDangerousMaterials = value; }
+        }
+    }
+}
---
+++ A16 Ex03/OverRefuelException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A16_Ex03
+{
+    class OverRefuelException : Exception
+    {
+        private float m_LitersToRefuel;
+        private Enums.eFuelType m_FuelType;
+
+        public OverRefuelException(float i_LitersToFool, Enums.eFuelType i_FuelType) :
+            base(
+            string.Format("Error occured while trying to refuel {0} into fuel type {1}", i_LitersToFool, i_FuelType))
+        {
+            m_LitersToRefuel = i_LitersToFool;
+            m_FuelType = i_FuelType;
+        }
+
+        public float LitersToRefuel
+        {
+            get { return m_LitersToRefuel; }
+        }
+
+        public Enums.eFuelType FuelType
+        {
+            get { return m_FuelType; }
+        }
+    }
+}
