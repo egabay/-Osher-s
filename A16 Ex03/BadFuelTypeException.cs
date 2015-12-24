32941b98630c801710853455753c21f788869856
osher <osherztikz@gmail.com>
2015-12-24 18:44:51 +0200

    Bad Fuel Type exc

---
+++ A16 Ex03/BadFuelTypeException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A16_Ex03
+{
+    class BadFuelTypeException : Exception
+    {
+        private eFuelType m_FuelTypeToFill;
+        private eFuelType m_FuelTypeRequired;
+        public BadFuelTypeException(eFuelType i_FuelTypeToFill, eFuelType i_FuelTypeRequired)
+            : base(String.Format("Error occured while trying to fill Fuel Type : {0} Into {1} Tank", i_FuelTypeToFill, i_FuelTypeRequired))
+        {
+            m_FuelTypeRequired = m_FuelTypeRequired;
+            m_FuelTypeToFill = i_FuelTypeToFill;
+        }
+        public eFuelType FuelTypeToFill
+        {
+            get { return m_FuelTypeToFill; }
+        }
+        public eFuelType FuelTypeRequired
+        {
+            get { return m_FuelTypeRequired; }
+        }
+    }
+}
