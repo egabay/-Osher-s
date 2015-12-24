43438df38bbdf8d4af590ceb5dbef85daea8faa5
osher <osherztikz@gmail.com>
2015-12-24 19:39:43 +0200

    sda

---
+++ A16 Ex03/Engine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A16_Ex03
+{
+
+    /*    בכלי רכב שעובד על דלק ניתן למצוא את המידע הבא ולהפעיל את הפעולות הבאות:
+     סוג הדלק ) Octan98 , Octan96 , Octan95 , Soler )
+     כמות הדלק הנוכחית בליטרים ) float )
+     כמות הדלק המקסימאלית בליטרים ) float )
+     פעולת תדלוק )מתודה שמקבלת כמות ליטרים להוספה וסוג דלק, ומשנה את מצב הדלק
+    במידה וסוג הדלק מתאים ואין חריגה מגודל הטנק(
+    בכלי רכב חשמלי ניתן למצוא את המידע הבא ולהפעיל את הפעולות הבאות:
+     זמן מצבר שנותר בשעות ) float )
+     זמן מצבר מקסימאלי בשעות ) float )
+     פעולת טעינת מצבר )מתודה שמקבלת נתון שהוא מספר שעות להוסיף למצבר ו"טוענת" את
+    המצבר בהתאמה כל עוד מספר השעות לא חורג מהמקסימום(*/
+    abstract public class Engine
+    {
+        protected float m_CurrentEnergyStorageStatus;
+        protected float m_MaximumEnergyStorageCapacity;
+        public float CurrentEnergyStorageStatus
+        {
+            get { return m_CurrentEnergyStorageStatus; }
+        }
+        public float MaximumEnergyStorageCapacity
+        {
+            get { return m_MaximumEnergyStorageCapacity; }
+        }
+        public Engine(float i_CurrentEnergyStorageStatus, float i_MaximumEnergyStorageCapacity)
+        {
+            m_CurrentEnergyStorageStatus = i_CurrentEnergyStorageStatus;
+            m_MaximumEnergyStorageCapacity = i_MaximumEnergyStorageCapacity;
+        }
+    }
+}
---
+++ A16 Ex03/FuelEngine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A16_Ex03
+{
+    enum eFuelType
+    {
+        Octan98,
+        Octan96,
+        Octan95,
+        Soler
+    }
+    class FuelEngine : Engine
+    {
+        eFuelType m_FuelType;
+        public FuelEngine(float i_CurrentEnergyStorageStatus, float i_MaximumEnergyStorageCapacity, eFuelType i_FuelType)
+            : base(i_CurrentEnergyStorageStatus, i_MaximumEnergyStorageCapacity)
+        {
+            m_FuelType = i_FuelType;
+        }
+        public void RefillEnergyStorage(float i_AmountEnergyToFill,eFuelType i_FuelType)
+        {
+            if(i_FuelType==m_FuelType)
+            {
+                if(i_AmountEnergyToFill<=m_MaximumEnergyStorageCapacity-m_CurrentEnergyStorageStatus)
+                {
+                    m_CurrentEnergyStorageStatus += i_AmountEnergyToFill;
+                }
+                else
+                {
+                    throw new OverEnergyStorageMaxException(i_AmountEnergyToFill, m_MaximumEnergyStorageCapacity - m_CurrentEnergyStorageStatus);
+                }
+            }
+            else
+            {
+                throw new BadFuelTypeException(i_FuelType, m_FuelType);
+            }
+        }
+    }
+}
