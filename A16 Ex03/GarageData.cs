e789c37b27d331c4ee5472a8e9cc035cc44a08d0
osher <osherztikz@gmail.com>
2015-12-26 21:15:44 +0200

    Check

--- A16 Ex03/GarageData.cs
+++ A16 Ex03/GarageData.cs
@@ -6,6 +6,12 @@ namespace Ex03.GarageLogic
 {
     class GarageData
     {
+        Dictionary<string,Vehicle> m_LicenseToVehicle=new Dictionary<string,Vehicle>();
+        Dictionary<string,VehicleOwner> m_LicenseToOwner=new Dictionary<string,VehicleOwner>();
 
+        public bool AddNewVehicle(Vehicle i_VehicleToAdd,VehicleOwner i_VehicleOwnerToAdd)
+        {
+            return true;
+        }
     }
 }
