78eaabbbd11c224a635626d0f0406a7e3399b5e6
egabay <edvin4112@gmail.com>
2015-12-30 23:10:12 +0200

    Interface done

---
+++ A16 EX04 EdvinGabay 201180577 OsheElmakaies 307934620/A16 EX04/Ex04.Menus.Interfaces/ActionItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class ActionItem : Menu
+    {
+        private readonly List<IActionListeners> r_ListenersInterfaceList;
+
+        public ActionItem(string i_MenuTitle, SubMenu i_Parent) : base(i_MenuTitle, i_Parent)
+        {
+            r_ListenersInterfaceList = new List<IActionListeners>();
+        }
+
+        private void OnSelection()
+        {
+            foreach (IActionListeners item in r_ListenersInterfaceList)
+            {
+                item.Operate();
+            }
+        }
+
+        public void AddActionListeners(IActionListeners i_ActionListeners)
+        {
+            if (i_ActionListeners != null)
+            {
+                r_ListenersInterfaceList.Add(i_ActionListeners);
+            }
+        }
+
+        public override void Show()
+        {
+            Console.Clear();
+            OnSelection();
+            Console.WriteLine(Environment.NewLine + "Press 'Enter' button for continue");
+            Console.ReadKey();
+        }
+    }
+}
