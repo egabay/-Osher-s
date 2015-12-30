65afff063994b5a44c4ad889be59a193cc1d9f65
egabay <edvin4112@gmail.com>
2015-12-30 21:20:26 +0200

    New

---
+++ A16 EX04 EdvinGabay 201180577 OsheElmakaies 307934620/A16 EX04/Ex04.Menus.Interfaces/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex04.Menus.Interfaces
+{
+    public abstract class Menu
+    {
+        protected readonly SubMenu r_Parent;
+        protected readonly string r_Title;
+
+        protected Menu(string i_Title, SubMenu i_Parent)
+        {
+            r_Title = i_Title;
+            r_Parent = i_Parent;
+        }
+
+        public SubMenu Parent
+        {
+            get { return r_Parent; }
+        }
+
+        public string Title
+        {
+            get { return r_Title; }
+        }
+
+        public abstract void Show();
+    }
+}
