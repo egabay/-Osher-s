5a111606f228849aa69ee8d01db104ae440310b1
egabay <edvin4112@gmail.com>
2016-01-06 14:03:25 +0200

    New

---
+++ A16 EX05 EdvinGabay 201180577 OsherElmakaies 307934620/A16 EX04 EdvinGabay 201180577 OsherElmakaies 307934620/A16 EX02/Board.cs
@@ -0,0 +1,146 @@
+﻿﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Ex02.ConsoleUtils;
+
+
+namespace Ex02_Baord
+{
+    public enum ePlayer
+    {
+        Empty,
+        O,
+        U,
+        X,
+        K,
+    };
+
+    // $G$ CSS-999 (-5) Every Class/Enum which is not nested should be in a separate file.
+    public enum eMoveDirection
+    {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight,
+    };
+
+    public enum eMoveType
+    {
+        None,
+        Move,
+        Eat,
+    }
+
+    public class Board
+    {
+        public const int k_SmallLetterToInt = 97;
+        public const int k_BigLetterToInt = 65;
+        public const int k_RegularMoveSteps = 1;
+        public const int k_EatMoveSteps = 2;
+        public int sizeOfTable;
+        private int m_NumberOfK = 0;
+        private int m_NumberOfU = 0;
+
+        protected internal ePlayer[,] m_TableMatrix;
+        private int m_NumberOfX = 0;
+        private int m_NumberOfO = 0;
+
+        public int NumberOfK
+        {
+            get { return m_NumberOfK; }
+        }
+
+        public int NumberOfU
+        {
+            get { return m_NumberOfU; }
+        }
+
+        public Board(int i_TableSize)
+        {
+            m_TableMatrix = new ePlayer[i_TableSize, i_TableSize];
+            sizeOfTable = i_TableSize;
+        }
+
+        public int NumberOfX
+        {
+            get { return m_NumberOfX; }
+        }
+
+        public int NumberOfO
+        {
+            get { return m_NumberOfO; }
+        }
+
+        public void ConvertToIndexNumbers(string i_MoveFromTo, out int o_MoveFromLineIndex, out int o_MoveFromRowIndex,
+            out int o_MoveToLineIndex, out int o_MoveToRowIndex)
+        {
+            string[] splittedMovementData = i_MoveFromTo.Split('>');
+            o_MoveFromLineIndex = (int)splittedMovementData[0][0] - k_BigLetterToInt;
+            o_MoveFromRowIndex = (int)splittedMovementData[0][1] - k_SmallLetterToInt;
+            o_MoveToLineIndex = (int)splittedMovementData[1][0] - k_BigLetterToInt;
+            o_MoveToRowIndex = (int)splittedMovementData[1][1] - k_SmallLetterToInt;
+        }
+
+        public ePlayer this[int i_RowIndex, int i_LineIndex]
+        {
+            get { return m_TableMatrix[i_RowIndex, i_LineIndex]; }
+        }
+
+        public void InitializeTable()
+        {
+            for (int i = 0; i < m_TableMatrix.GetLength(0) / 2 - 1; i++)
+            {
+                for (int j = 0; j < m_TableMatrix.GetLength(1); j++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        m_TableMatrix[i, j] = ePlayer.O;
+                        j++;
+                    }
+                    else
+                    {
+                        j++;
+                        m_TableMatrix[i, j] = ePlayer.O;
+                    }
+                    m_NumberOfO++;
+                }
+            }
+            for (int i = m_TableMatrix.GetLength(0) - 1; i > m_TableMatrix.GetLength(0) / 2; i--)
+            {
+                for (int j = 0; j < m_TableMatrix.GetLength(1); j++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        m_TableMatrix[i, j] = ePlayer.X;
+                        j++;
+                    }
+                    else
+                    {
+                        j++;
+                        m_TableMatrix[i, j] = ePlayer.X;
+                    }
+                    m_NumberOfX++;
+                }
+            }
+        }
+
+        public string ConvertToString(int i_MoveFromRowIndex, int i_MoveToRowIndex, int i_MoveFromLineIndex,
+            int i_MoveToLineIndex, string[] o_ArrayOfEatingPossitions, int i_indexForArray)
+        {
+            char fromLocationBigLetter = (char)(i_MoveFromLineIndex + k_BigLetterToInt);
+            o_ArrayOfEatingPossitions[i_indexForArray] = fromLocationBigLetter.ToString();
+            char fromLocationSmallLetter = (char)(i_MoveFromRowIndex + k_SmallLetterToInt);
+            o_ArrayOfEatingPossitions[i_indexForArray] += fromLocationSmallLetter.ToString();
+            o_ArrayOfEatingPossitions[i_indexForArray] += ">";
+            char toLocationBigLetter = (char)(i_MoveToLineIndex + k_BigLetterToInt);
+            o_ArrayOfEatingPossitions[i_indexForArray] += toLocationBigLetter.ToString();
+            char toLocationSmallLetter = (char)(i_MoveToRowIndex + k_SmallLetterToInt);
+            o_ArrayOfEatingPossitions[i_indexForArray] += toLocationSmallLetter.ToString();
+            return o_ArrayOfEatingPossitions[i_indexForArray];
+        }
+
+
+    }
+}
+
---
+++ A16 EX05 EdvinGabay 201180577 OsherElmakaies 307934620/A16 EX04 EdvinGabay 201180577 OsherElmakaies 307934620/A16 EX02/PlayerInfo.cs
@@ -0,0 +1,123 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Text;
+﻿using Ex02_Baord;
+
+
+namespace Ex02_New
+{
+    class PlayerInfo
+    {
+        private string m_Name;
+        private int m_NumberOfWins;
+        private int m_Score; 
+        private ePlayer m_NormalSign;
+        private ePlayer m_KinglSign;
+        public int PawnsNumber { get; private set; }
+
+        public int Score
+        {
+            get { return m_Score; }
+            set { m_Score = value; }
+        }
+        public PlayerInfo(string i_Name, ePlayer i_ENormalPlayerSign, ePlayer i_EKingPlayerSign)
+        {
+            Name = i_Name;
+            ENormalSign = i_ENormalPlayerSign;
+            EKinglSign = i_EKingPlayerSign;
+            PlayerAmountOfWins = 0;
+        }
+
+        public ePlayer ENormalSign
+        {
+            get { return m_NormalSign; }
+            set { m_NormalSign = value; }
+        }
+
+        public ePlayer EKinglSign
+        {
+            get { return m_KinglSign; }
+            set { m_KinglSign = value; }
+        }
+
+        public string Name
+        {
+            get { return m_Name; }
+            set { m_Name = value; }
+        }
+
+        public int PlayerAmountOfWins
+        {
+            get
+            {
+                return m_NumberOfWins;
+            }
+            set { m_NumberOfWins = value; }
+        }
+
+        public void UpdatePawnsNumber(int i_Number)
+        {
+            PawnsNumber = i_Number;
+        }
+
+        public bool CanEatAgain(int i_MoveFromLineIndex, int i_MoveFromRowIndex, Board m_Table, ePlayer i_ESign)
+        {
+            bool answerForEatingAgain = false;
+            string currentLocation = m_Table[i_MoveFromRowIndex, i_MoveFromLineIndex].ToString();
+            string bottomRight = m_Table[i_MoveFromRowIndex + 1, i_MoveFromLineIndex + 1].ToString();
+            string bottomLeft = m_Table[i_MoveFromRowIndex + 1, i_MoveFromLineIndex - 1].ToString();
+            string topLeft = m_Table[i_MoveFromRowIndex - 1, i_MoveFromLineIndex + 1].ToString();
+            string topRight = m_Table[i_MoveFromRowIndex - 1, i_MoveFromLineIndex - 1].ToString();
+            if (CheckForOpponentAroundYou(currentLocation, bottomRight))
+            {
+                if (CheckEmptyAroundYou(bottomRight))
+                {
+                    answerForEatingAgain = true;
+                }
+            }
+            if (CheckForOpponentAroundYou(currentLocation, bottomLeft))
+            {
+                if (CheckEmptyAroundYou(bottomLeft))
+                {
+                    answerForEatingAgain = true;
+                }
+            }
+            if (CheckForOpponentAroundYou(currentLocation, topLeft))
+            {
+                if (CheckEmptyAroundYou(topLeft))
+                {
+                    answerForEatingAgain = true;
+                }
+            }
+            if (CheckForOpponentAroundYou(currentLocation, topRight))
+            {
+                if (CheckEmptyAroundYou(topRight))
+                {
+                    answerForEatingAgain = true;
+                }
+            }
+            // $G$ CSS-006 (-3) missing blank line.
+            return answerForEatingAgain;
+
+        }
+
+        public bool CheckForOpponentAroundYou(string i_CurrentLocation, string i_OpponentLocation)
+        {
+            bool answerForOpponent = false;
+            ePlayer opponentSign;
+            opponentSign = (ENormalSign == ePlayer.X) ? ePlayer.O : ePlayer.X;
+            if (i_CurrentLocation != opponentSign.ToString())
+            {
+                answerForOpponent = true;
+            }
+            return answerForOpponent;
+        }
+
+        public bool CheckEmptyAroundYou(string i_CurrentLocation)
+        {
+
+            bool answerForEmptyPlace = i_CurrentLocation.Equals(ePlayer.Empty.ToString());
+            return answerForEmptyPlace;
+        }
+    }
+}
