d9e883b315424092b4cb55b79dc94a05861c8936
egabay <edvin4112@gmail.com>
2016-01-07 21:00:02 +0200

    1

---
+++ A16 EX05 EdvinGabay 201180577 OsherElmakaies 307934620/A16 EX04 EdvinGabay 201180577 OsherElmakaies 307934620/A16 EX02/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ex02_New;
+
+namespace Ex02_GameManager
+{
+    public class GameManager
+    {
+        public Board FirstBoard = new Board(8);
+        
+
+    }
+}
